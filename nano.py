bfb5d1cafdb0871b244dd2b06ddc4f605054958b
Kazuo Hirota <khrtota@gmail.com>
2018-09-02 00:14:07 +0900

    import 文を変更 　import tkinter as t とし、Label, PhotoImageを　t.Label t.PhotoImageで使用

--- nano.py
+++ nano.py
@@ -4,7 +4,9 @@ import tkinter as tk
 root = tk.Tk()
 
 img = tk.PhotoImage(file='nanohana.gif')
-
+img2 = tk.PhotoImage(file='happy.gif')
 label = tk.Label(root, image=img)
+label2 = tk.Label(root, image=img2)
 label.grid()
+label2.grid()
 root.mainloop()
