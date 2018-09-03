3514e23b55de1c203a0fa51de18aaff76f431dfa
Kazuo Hirota <khrtota@gmail.com>
2018-09-03 22:03:48 +0900

    calc-logic.py  GUI + Logic 終了　（”80行”電卓）

--- calc-logic.py
+++ calc-logic.py
@@ -4,23 +4,24 @@ import tkinter.messagebox
 
 root = tkinter.Tk()
 root.geometry('300x270+400+100')
+root.resizable(False,False)
 root.title('Calculator')
 
 contentVar = tkinter.StringVar(root, '')
-contentEntry = tkeinter.Entry(root,textvaliable = contentVar)
-contentEntry['State'] = 'readonly'
+contentEntry = tkinter.Entry(root,textvariable = contentVar)
+contentEntry['state'] = 'readonly'
 contentEntry.place(x=10, y=10, width=280, height=20)
 
 
 def buttonClick(btn):
     content = contentVar.get()
-    if content.stratwith('.'):
+    if content.startswith('.'):
         content = '0' + content
-    if btn in '0123456789'
+    if btn in '0123456789':
         content += btn
     elif btn == '.':
         lastPart = re.split(r'\+|-|\*|/]', content)[-1]
-        if '.' in lastPart
+        if '.' in lastPart:
             tkinter.messagebox.showerror('Error','Too many periods')
             return
         else:
@@ -34,6 +35,43 @@ def buttonClick(btn):
             tkinter.messagebox.showerror('Error','Expression error')
             return
     elif btn in operators:
+        if content.endswith(operators):
+            tkinter.messagebox.showerror('Error', 'Multiple Operators' )
+            return
+        content += btn
+    elif btn == 'Sqrt':
+        n = content.spilit('.')
+        if all(map(lambda x: x.isdigit(), n)):
+            content = eval(content) ** 0.5
+        else:
+            tkinter.messagebox.showerror('Error', 'Expression Error')
+            return
+
+    contentVar.set(content)
+#---------------
+
+btnClear = tkinter.Button(root, text="Clear", command=lambda:buttonClick('C'))
+btnClear.place(x=40, y=40, width=80, height=20)
+btnCompute = tkinter.Button(root, text='=', command=lambda:buttonClick('='))
+btnCompute.place(x=170, y=40, width=80, height=20)
+
+digits = list('0123456789.')+['Sqrt']
+index = 0
+for row in range(4):
+    for col in range(3):
+        d = digits[index]
+        index += 1
+        btnDigit = tkinter.Button(root, text = d, command=lambda x=d:buttonClick(x))
+        btnDigit.place(x=20+col*70, y=80+row*50, width=50, height=20)
+
+operators = ('+','-','*','/','**','//')
+for index, operator in enumerate(operators):
+    btnOperator = tkinter.Button(root,text=operator, command=lambda x=operator:buttonClick(x))
+    btnOperator.place(x=230, y=80+index*30, width=50, height=20)
+
+
+root.mainloop()
+
 
 
 
