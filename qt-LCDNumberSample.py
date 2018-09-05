0f02ee99dd08f8199c2ce110fc387c4c19b48daf
Kazuo Hirota <khrtota@gmail.com>
2018-09-05 14:58:48 +0900

    PyQt  QLCDNumber ウィジェットを試す。あまり綺麗ではない。

---
+++ qt-LCDNumberSample.py
@@ -0,0 +1,25 @@
+import sys
+
+from PySide2.QtWidgets import QApplication, QPushButton, QLCDNumber
+
+
+
+# Create the Qt Application
+
+app = QApplication(sys.argv)
+
+# Create a button
+
+button = QLCDNumber()
+
+button.display(123)
+
+# Show the button
+
+button.show()
+
+# Run the main Qt loop
+
+app.exec_()
+
+
