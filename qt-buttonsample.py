4e145f5b271112ad5ee3d1b4cf39c686e8f7e9c0
Kazuo Hirota <khrtota@gmail.com>
2018-09-05 09:53:16 +0900

    PyQt 使用開始

---
+++ qt-buttonsample.py
@@ -0,0 +1,29 @@
+import sys
+
+from PySide2.QtWidgets import QApplication, QPushButton
+
+# Greetings
+
+def say_hello():
+
+    print("Button clicked, Hello!")
+
+# Create the Qt Application
+
+app = QApplication(sys.argv)
+
+# Create a button
+
+button = QPushButton("Click me")
+
+# Connect the button to the function
+button.clicked.connect(say_hello)
+
+# Show the button
+
+button.show()
+
+# Run the main Qt loop
+
+app.exec_()
+
