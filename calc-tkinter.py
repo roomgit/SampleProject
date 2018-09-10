53d4cf00a934be8bae943470db76c283c1c2dbf0
Kazuo Hirota <khrtota@gmail.com>
2018-09-10 13:07:31 +0900

    Tkinter  calc-tkinter いろいろ整理（どのタイミングでtext=''を行うか？？）

--- calc-tkinter.py
+++ calc-tkinter.py
@@ -89,40 +89,44 @@ class window(tk.Frame):
         buttons[14].place(x=100, y=80, width=30, height=20)  # multiply
         buttons[15].place(x=100, y=100, width=30, height=20)  # divide
         buttons[16].place(x=130, y=40, width=120, height=61)  # all_clear
-        #period.place(x=40, y=100, width=30, height=20)
-        #equal.place(x=70, y=100, width=30, height = 20)
-        #plus.place(x=100, y=40, width=30, height=20)
-        #minus.place(x=100, y=60, width=30, height=20)
-        #multiply.place(x=100, y=80, width=30, height=20)
-        #divide.place(x=100, y=100, width=30, height=20)
-        #all_clear.place(x=130, y=40, width=120, height=61)
+
 
         self.wait_initial_input = True
         self.work = 0
         self.operation = ""
 
 
+    def digit_entry(self, btn):
+
+        if self.wait_initial_input == True:
+            self.work = float(self.contentVar.get())
+            print("inital")
+            print(self.work)
+            contents = btn
+            self.contentVar.set(contents)
+            self.wait_initial_input = False
+        else:
+            self.work = float(self.contentVar.get())
+            contents = self.contentVar.get() + btn
+
+        self.contentVar.set(contents)
+
     def calculate(self,btn):
         print(btn + 'was operated')
 
         if btn == '+':
-            #event.widget.configure(image=self.press_img[12])
             self.wait_initial_input = True
             self.operation = '+'
         elif btn == '-':
-            #event.widget.configure(image=self.press_img[13])
             self.wait_initial_input = True
             self.operation = '-'
         elif btn == '*':
-            #event.widget.configure(image=self.press_img[14])
             self.wait_initial_input = True
             self.operation = '*'
         elif btn == '/':
-            #event.widget.configure(image=self.press_img[15])
             self.wait_initial_input = True
             self.operation = '/'
         elif btn == '=':
-            #event.widget.configure(image=self.press_img[10])
             if self.operation == '+':
                 contents = self.work + float(self.contentVar.get())
             elif self.operation == '-':
@@ -139,6 +143,7 @@ class window(tk.Frame):
             self.operation = ''
             self.work = 0
 
+
     def btn_press(self, event):
         btn = event.widget["text"]
         event.widget.configure(image=self.press_img[int(btn)])
@@ -146,38 +151,30 @@ class window(tk.Frame):
 
         if btn in list:
             print(btn)
-
-            if self.wait_initial_input == True:
-                self.work = float(self.contentVar.get())
-                print("inital")
-                print(self.work)
-                contents = btn
-                self.contentVar.set(contents)
-                self.wait_initial_input = False
-            else:
-                self.work = float(self.contentVar.get())
-                contents= self.contentVar.get() + btn
-
-            self.contentVar.set(contents)
-
-        elif btn == '+':
-           print('plus button')
-        elif btn == '-':
-           print('minus button')
-        elif btn == '*':
-           print('multiply button')
-        elif btn == '/':
-           print(' divide button')
-        elif btn == '=':
-           print('equal button')
-        elif btn == 'C':
+            self.digit_entry(btn)
+        elif btn == '10':
+            print('period button')
+            self.digit_entry(btn)
+        elif btn == '11':
+            print('equal button')
+            self.calculate(btn)
+        elif btn == '12':
+            print('plus button')
+            self.calculate(btn)
+        elif btn == '13':
+            print('muinus button')
+            self.calculate(btn)
+        elif btn == '14':
+            print('multiply button')
+            self.calculate(btn)
+        elif btn == '15':
+            print('divide button')
+            self.calculate(btn)
+        elif btn == '16':
            print('all clear button')
+           self.all_clear()
         else:
             print('other buttons')
-
-    def button_release(self,event):
-        btn = event.widget["text"]
-        event.widget.configure(image=self.release_img[int(btn)])
 
 
     def period_press(self,event):
@@ -191,55 +188,16 @@ class window(tk.Frame):
 
         self.calculate(b)
 
-        '''
-        if btn == '+':
-            event.widget.configure(image=self.press_img[12])
-            self.wait_initial_input = True
-            self.operation = '+'
-        elif btn == '-':
-            event.widget.configure(image=self.press_img[13])
-            self.wait_initial_input = True
-            self.operation = '-'
-        elif btn == '*':
-            event.widget.configure(image=self.press_img[14])
-            self.wait_initial_input = True
-            self.operation = '*'
-        elif btn == '/':
-            event.widget.configure(image=self.press_img[15])
-            self.wait_initial_input = True
-            self.operation = '/'
-        elif btn == '=':
-            event.widget.configure(image=self.press_img[10])
-            if self.operation == '+':
-                contents = self.work + float(self.contentVar.get())
-            elif self.operation == '-':
-                contents = self.work - float(self.contentVar.get())
-            elif self.operation == '*':
-                contents = self.work * float(self.contentVar.get())
-            elif self.operation == '/':
-                contents = self.work / float(self.contentVar.get())
-            else:
-                contents = float(self.contentVar.get())
 
-            self.contentVar.set(contents)
-            self.wait_initial_input = True
-            self.operation = ''
-            self.work = 0
-        '''
 
     def btn_release(self, event):
         event.widget.configure(image=self.release_img[int(event.widget["text"])])
 
-    def all_clear_press(self, event):
-        event.widget.configure(image=self.press_img[16])
+    def all_clear(self):
+        #event.widget.configure(image=self.press_img[16])
         self.saved_operator='None'
         self.work=0
         self.contentVar.set('0')
-
-
-
-
-
 
 
 if __name__ == "__main__":
