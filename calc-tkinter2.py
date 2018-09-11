7c02d0a1bbc116bb988ff22a90f9996e8a31fc76
Kazuo Hirota <khrtota@gmail.com>
2018-09-11 22:05:34 +0900

    Tkinter  calc-tkinter C ボタン追加　（表示の桁規制　処理未対応）

--- calc-tkinter2.py
+++ calc-tkinter2.py
@@ -5,28 +5,32 @@ import tkinter as tk
 class window(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master,  width=722, height=375)
+        self.master.title('Calc')
+        self.master.resizable(False, False)
         self.img = tk.PhotoImage(file='./resource/background.png')
         label = tk.Label(self, image=self.img)
         label.place(x=0, y=0, width=722, height=375)
-        self.master.title('Calc')
-        self.master.resizable(False, False)
 
         # Display Contents
         self.contentVar = tk.StringVar(self, '')
-        contentEntry = tk.Entry(self, textvariable=self.contentVar, font="Helvetica 36 bold", justify='right')
+        contentEntry = tk.Entry(self, textvariable=self.contentVar, font="Helvetica 36 bold ", justify='right')
         contentEntry['state'] = 'readonly'
         contentEntry.place(x=211, y=20, width=300, height=70)
         content = "12345.678"
         self.contentVar.set(content)
 
+        # Error icon
+        self.err_icon = tk.Label(self, font="Helvetica 10", text='')
+        self.err_icon.place(x=213, y=24, width=10, height=10)
+
         # Buttons
         buttons = []
-        for i in range(0,18):
+        for i in range(0, 18):
             button = tk.Button(self, text=str(i), bd=0)
             buttons.append(button)
 
         # Binding
-        for i in range(18):
+        for i in range(0, 18):
             buttons[i].bind("<Button-1>", self.btn_press)
             buttons[i].bind("<ButtonRelease-1>", self.btn_release)
 
@@ -38,7 +42,7 @@ class window(tk.Frame):
             self.press_img.append(tk.PhotoImage(file=filename))
             filename = './resource/Release_%d.png' % n
             self.release_img.append(tk.PhotoImage(file=filename))
-        for n in range(0,18):
+        for n in range(0, 18):
             buttons[n].configure(image=self.release_img[n])  # images of released buttons
 
         # Layout
@@ -69,36 +73,27 @@ class window(tk.Frame):
     def btn_press(self, event):
         btn = event.widget["text"]
         event.widget.configure(image=self.press_img[int(btn)])  # images of pressed buttons
-        list01 = ("0","1","2","3","4","5","6","7","8","9")
-        if btn in list01:
-            print(btn)
+        list0 = ("0","1","2","3","4","5","6","7","8","9")
+        if btn in list0:
             self.digit_entry(btn)
         elif btn == '10':
-            print('period button')
             self.period_entry()
         elif btn == '11':
-            print('equal button')
-            self.operator_entry("=")
-        elif btn == '12':
-            print('plus button')
+            self.equal_entry()
+        elif btn in '12':
             self.operator_entry("+")
         elif btn == '13':
-            print('minus button')
             self.operator_entry("-")
         elif btn == '14':
-            print('multiply button')
             self.operator_entry("*")
         elif btn == '15':
-            print('divide button')
             self.operator_entry("/")
         elif btn == '16':
-            print('all clear button')
             self.ac_entry()
         elif btn == '17':
-            print('clear button')
             self.c_entry()
-        else:
-            print('other buttons')
+        #else:
+            #print('other buttons')
 
     def btn_release(self, event):
         event.widget.configure(image=self.release_img[int(event.widget["text"])])
@@ -108,9 +103,8 @@ class window(tk.Frame):
     # Calculator
     #
     def digit_entry(self, btn):
+        self.err_icon.configure(text="")
         if self.wait_initial_input:  # 初回キー入力待ち
-            print("inital")
-            print(self.work)
             contents = btn
             self.contentVar.set(contents)
             self.wait_initial_input = False
@@ -122,47 +116,25 @@ class window(tk.Frame):
         self.contentVar.set(contents)
 
     def period_entry(self):
+        self.err_icon.configure(text="")
         if self.wait_initial_input:
             contents = "0."
             self.wait_initial_input = False
         elif '.' not in (self.contentVar.get()):  # 小数点の複数回入力抑止
             contents = self.contentVar.get() + "."
-        self.contentVar.set(contents)
+            self.contentVar.set(contents)
 
     def operator_entry(self, btn):
-        #btn = event.widget['text']
-        print(btn + 'was operated')
-        if btn == '+':
-            if not self.wait_initial_input:  # 演算子の押しなおしでない
-                self.calculate()
-            self.work = float(self.contentVar.get())
-            self.wait_initial_input = True
-            print(self.operation)
-            print("work is " + str(self.work))
-            self.operation = '+'
-        elif btn == '-':
-            if not self.wait_initial_input:  # 演算子の押しなおしでない
-                self.calculate()
-            self.work = float(self.contentVar.get())
-            self.wait_initial_input = True
-            self.operation = '-'
-        elif btn == '*':
-            if not self.wait_initial_input:  # 演算子の押しなおしでない
-                self.calculate()
-            self.work = float(self.contentVar.get())
-            self.wait_initial_input = True
-            self.operation = '*'
-        elif btn == '/':
-            if not self.wait_initial_input:  # 演算子の押しなおしでない
-                self.calculate()
-            self.work = float(self.contentVar.get())
-            self.wait_initial_input = True
-            self.operation = '/'
-        elif btn == '=':
-
+        if not self.wait_initial_input:  # '演算子の押しなおし'でない
             self.calculate()
-            self.wait_initial_input = True
-            #self.operation = ""
+        self.work = float(self.contentVar.get())
+        self.wait_initial_input = True
+        self.operation = btn
+
+    def equal_entry(self):
+        self.calculate()
+        self.wait_initial_input = True
+        self.operation = ""
 
     def calculate(self):
         contents = float(self.contentVar.get())
@@ -174,7 +146,7 @@ class window(tk.Frame):
             contents = self.work * contents
         elif self.operation == '/':
             if contents == 0:
-                pass
+                self.err_icon.configure(text="E")
             else:
                 contents = self.work / contents
 
@@ -185,12 +157,14 @@ class window(tk.Frame):
             contents = d[0]   # ゼロの左側のみを採用
 
         if len(d[0]) > 10:  # 整数部分が　10 桁を超えるとエラー
-            contents = "error"
+            self.err_icon.configure(text="E")
+            contents = "0"
         self.contentVar.set(contents)
 
 
 
     def ac_entry(self):
+        self.err_icon.configure(text="")
         self.operation = ''
         self.work = 0
         self.contentVar.set('0')
